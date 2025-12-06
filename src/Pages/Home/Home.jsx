1f789bae5861b691b9cbdb0117986b5cbde89864
HossainShahRahat <shah.rahat.hossain.aiub@gmail.com>
2025-12-07 01:50:33 +0600

    add initial home page component

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-bold mt-10">
+        Welcome to AssetVerse
+      </h1>
+    </div>
+  );
+};
+
+export default Home;

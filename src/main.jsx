dfc92b622fb4779f6fd0ff51e2587aa2a16f0b04
HossainShahRahat <shah.rahat.hossain.aiub@gmail.com>
2025-12-07 01:52:39 +0600

    updated StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import router from "./Routes/Routes.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </StrictMode>
 );

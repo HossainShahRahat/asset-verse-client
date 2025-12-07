d299ee2e73c458b5ed5c6cdef5a4b9233a119a58
HossainShahRahat <shah.rahat.hossain.aiub@gmail.com>
2025-12-08 01:23:16 +0600

    refactor: add packages section to home page layout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "./About";
 import Banner from "./Banner";
+import Packages from "./Packages";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <About />
+      <Packages />
     </div>
   );
 };

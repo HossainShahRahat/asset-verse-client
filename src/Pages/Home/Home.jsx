e8618b07dfb2952c32b7ffa6816e8790c6523666
HossainShahRahat <shah.rahat.hossain.aiub@gmail.com>
2025-12-09 04:25:09 +0600

    refactor: assemble full home page with all feature sections

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,27 @@
 import About from "./About";
 import Banner from "./Banner";
+import Features from "./Features";
 import Packages from "./Packages";
+import Testimonials from "./Testimonials";
+import HowItWorks from "./HowItWorks";
+import FaqSection from "./FaqSection";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <div>
+      {/* Added Helmet for page title if you have react-helmet installed, otherwise remove */}
+      <Helmet>
+        <title>AssetVerse | Home</title>
+      </Helmet>
+
       <Banner />
       <About />
+      <HowItWorks />
+      <Features />
       <Packages />
+      <Testimonials />
+      <FaqSection />
     </div>
   );
 };
